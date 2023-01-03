daf3a080512fd9547675e70766482d93cbfc8726
dudujunior12 <dudu12310@hotmail.com>
2023-01-02 22:04:08 -0300

    feat: requirements.txt and .gitignore

--- tsurei/settings.py
+++ tsurei/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
+import environ
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$_dx34ceyz*4(&if@*^-nnv1ofwl+2bgo@n7!orea5+ea3(@sm'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
