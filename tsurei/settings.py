34baa78f644bab334ec9438630cbb9bd3ce50f4c
dudujunior12 <dudu12310@hotmail.com>
2023-02-18 10:39:55 -0300

    fix: env secret key

--- tsurei/settings.py
+++ tsurei/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import dotenv
 from pathlib import Path
 
 import environ
@@ -20,6 +21,10 @@ environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+#dotenv
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
