33fad8a063e26d7c6452fe2bf42b7f72a17253c0
dudujunior12 <dudu12310@hotmail.com>
2023-04-07 21:35:34 -0300

    update: fix carrousel image size

--- manga/static/js/profile.js
+++ manga/static/js/profile.js
@@ -7,13 +7,3 @@ var swiper1 = new Swiper(".swiper1", {
         clickable: true,
     },
 });
-
-var swiper2 = new Swiper(".swiper2", {
-    slidesPerView: 5,
-    slidesPerGroup: 5,
-    spaceBetween: 50,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-});
