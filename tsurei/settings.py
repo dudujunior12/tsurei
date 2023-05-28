8163b687f77685289ff8d9554afbc6462b47bc0e
Eduardo dos Santos <68609826+dudujunior12@users.noreply.github.com>
2023-05-28 20:26:11 -0300

    update: settings.py

--- tsurei/settings.py
+++ tsurei/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '*', '127.0.0.1']
 
 
 # Application definition
@@ -96,14 +96,13 @@ AUTH_USER_MODEL = 'manga.User'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': env("DATABASE_NAME"),
-        'USER': env("USER_NAME"),
-        'PASSWORD': env("PASSWORD"),
-        'HOST': env("HOST_ADDRESS"),
-        'PORT': env("PORT"),
+        'ENGINE': 'django.db.backends.postgresql',
     }
 }
+
+ 	
+DATABASES['default'] = dj_database_url.config(default=env("DATABASE"))
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
