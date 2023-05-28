fa8bf88d8700f023899dd8706b0a9d3c22779b2c
Eduardo dos Santos <68609826+dudujunior12@users.noreply.github.com>
2023-05-28 20:36:36 -0300

    update: settings.py

--- tsurei/settings.py
+++ tsurei/settings.py
@@ -35,7 +35,7 @@ if os.path.isfile(dotenv_file):
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = ['.vercel.app', '*', '127.0.0.1']
 
