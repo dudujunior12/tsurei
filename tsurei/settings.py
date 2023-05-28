042dc557e72b3c28f607e64588fa1edc609371a6
Eduardo dos Santos <68609826+dudujunior12@users.noreply.github.com>
2023-05-28 10:00:26 -0300

    update: changed vercel allowed host

--- tsurei/settings.py
+++ tsurei/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.15.28', '127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
