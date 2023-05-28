3aa1822264ad2dbe763b568a044e0943a6fde1c1
Eduardo dos Santos <68609826+dudujunior12@users.noreply.github.com>
2023-05-28 12:03:34 -0300

    update: create build.sh

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Building the project..."
+python3.9 -m pip install -r requirements.txt
+
+echo "Running the project"
+python3.9 manage.py makemigrations manga --noinput
+python3.9 manage.py migrate --noinput
+
+echo "Collect Static"
+python3.9 manage.py collectstatic --noinput --clear
