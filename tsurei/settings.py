f5f987b037caf2796201e8eea0c09facf419254f
Eduardo dos Santos <68609826+dudujunior12@users.noreply.github.com>
2023-05-28 14:14:25 -0300

    update: settings.py

--- tsurei/settings.py
+++ tsurei/settings.py
@@ -143,9 +143,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
 
-#MEDIA_URL = '/images/'
+MEDIA_URL = '/images/'
 
-#MEDIA_ROOT = os.path.join(BASE_DIR, 'manga/static/images')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'manga/static/images')
 
 LOGIN_URL = '/login/'
 
