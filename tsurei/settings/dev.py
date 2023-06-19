72e81778b51506d6d9f286dc10b85fd2e841bea8
dudujunior12 <dudu12310@hotmail.com>
2023-06-19 00:21:03 -0300

    fix: static root url

--- tsurei/settings/dev.py
+++ tsurei/settings/dev.py
@@ -10,7 +10,5 @@ DATABASES = {
     }
 }
 
-
-
-
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
