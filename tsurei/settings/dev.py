43dbe78e43f1bf244399cc89b525c958d6b7f1e7
dudujunior12 <dudu12310@hotmail.com>
2023-06-25 17:20:41 -0300

    style: fix screen sizes

--- tsurei/settings/dev.py
+++ tsurei/settings/dev.py
@@ -4,7 +4,7 @@ from .base import *
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
 
 # Application definition
 
