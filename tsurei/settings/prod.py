ef2cc96841e4889a03e4a104619d1b3c5a3e491e
dudujunior12 <dudu12310@hotmail.com>
2023-06-27 20:17:05 -0300

    update: vercel settings

--- tsurei/settings/prod.py
+++ tsurei/settings/prod.py
@@ -1,9 +1,9 @@
 from .base import *
 
 import dj_database_url
-import cloudinary
-import cloudinary.uploader
-import cloudinary.api
+#import cloudinary
+#import cloudinary.uploader
+#import cloudinary.api
 
 # Application definition
 
@@ -12,7 +12,6 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 INSTALLED_APPS = [
     'whitenoise.runserver_nostatic',
     'manga',
-    'cloudinary',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -29,12 +28,12 @@ DATABASES = {
 
 DATABASES['default'] = dj_database_url.config(default=env("POSTGRES_URL"))
 
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': env('CLOUD_NAME'),
-    'API_KEY': env("API_KEY"),
-    'API_SECRET': env('API_SECRET'),
-}
+#CLOUDINARY_STORAGE = {
+#    'CLOUD_NAME': env('CLOUD_NAME'),
+#    'API_KEY': env("API_KEY"),
+#    'API_SECRET': env('API_SECRET'),
+#}
 
-DEFAULT_FILE_STORAGE = env("DEFAULT_FILE_STORAGE")
+#DEFAULT_FILE_STORAGE = env("DEFAULT_FILE_STORAGE")
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
