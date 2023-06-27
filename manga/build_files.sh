bc70372745baa424022734efa19c3d8d2cd720b6
dudujunior12 <dudu12310@hotmail.com>
2023-06-27 20:37:11 -0300

    fix: vercel settings

--- manga/build_files.sh
+++ manga/build_files.sh
@@ -1,2 +1,20 @@
-pip install -r requirements.txt
-python3.9 manage.py collectstatic
+echo "Building the project..."
+python3.9 -m pip install -r requirements.txt
+
+echo "Running the project"
+python3.9 manage.py makemigrations manga --noinput
+python3.9 manage.py migrate --noinput
+
+echo "Creating super user"
+
+DJANGO_SUPERUSER_EMAIL=${DJANGO_SUPERUSER_EMAIL}
+DJANGO_SUPERUSER_USERNAME=${DJANGO_SUPERUSER_USERNAME}
+DJANGO_SUPERUSER_PASSWORD=${DJANGO_SUPERUSER_PASSWORD}
+
+python3.9 manage.py createsuperuser \
+    --email $DJANGO_SUPERUSER_EMAIL \
+    --username $DJANGO_SUPERUSER_USERNAME \
+    --noinput || true
+
+echo "Collect Static"
+python3.9 manage.py collectstatic --noinput --clear
