9f133c30648bf03a4bb5b48685f1bed8d53c1903
Eduardo dos Santos <68609826+dudujunior12@users.noreply.github.com>
2023-07-19 16:00:36 -0300

    delete: manga directory

--- manga/static/js/chapter-nav.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded',function() {
-    document.querySelector('select[select-option]').onchange=changeEventHandler;
-},false);
-
-function changeEventHandler(event) {
-    value = this.options[this.selectedIndex].value;
-    value = value.match(/\d+/g);
-    window.location.href = value[0];
-}
--- manga/static/js/mobile-nav.js
+++
@@ -1,6 +0,0 @@
-const header = document.querySelector('.header');
-
-const btnNav = document.querySelector('.btn-mobile-nav')
-btnNav.addEventListener('click', function(){
-    header.classList.toggle('nav-open');
-})
