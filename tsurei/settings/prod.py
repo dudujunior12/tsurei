98793a997b232bc17d907ca6b545cdb3d1a2237b
Eduardo dos Santos <dudu12310@hotmail.com>
2023-09-18 13:50:54 -0300

    update: removed cloudinary settings

--- tsurei/settings/prod.py
+++ tsurei/settings/prod.py
@@ -1,38 +1,27 @@
 from .base import *
 
 import dj_database_url
-import cloudinary
-import cloudinary.uploader
-import cloudinary.api
 
 # Application definition
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".vercel.app"]
 
 INSTALLED_APPS = [
-    'manga',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "manga",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
 ]
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
     }
 }
 
-DATABASES['default'] = dj_database_url.config(default=env("POSTGRES_URL"))
-
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': env('CLOUD_NAME'),
-    'API_KEY': env("API_KEY"),
-    'API_SECRET': env('API_SECRET'),
-}
-
-DEFAULT_FILE_STORAGE = env("DEFAULT_FILE_STORAGE")
+DATABASES["default"] = dj_database_url.config(default=env("POSTGRES_URL"))
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
